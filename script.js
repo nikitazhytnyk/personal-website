34127ad5cc31ad3206c5fc5d78947b23eb45bae9
nikitazhytnyk <zhytnyk.nikita@gmail.com>
2023-08-15 11:45:10 +0300

    Fix script for active nav link on scroll

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    section.forEach(sec => {
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 200;
         let height = sec.offsetHeight;
@@ -11,7 +11,7 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
     });
